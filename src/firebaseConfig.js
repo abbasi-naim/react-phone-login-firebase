712402a144f2d2c48b881a599bcd2c9e54f3d604
abbasi-naim <abbasi.naim@gmail.com>
2022-01-09 23:47:34 +0300

    added firebaseconfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBApEml5ZZ7_OQ25FlEzLUsTqH6go98Wqs",
+  authDomain: "alert-howl-138923.firebaseapp.com",
+  projectId: "alert-howl-138923",
+  storageBucket: "alert-howl-138923.appspot.com",
+  messagingSenderId: "579553416758",
+  appId: "1:579553416758:web:f2d173a46115682052aa97",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
